02b5477744858619c3caf58fee51da08671f7715
John Ludlow <john.ludlow@halfords.co.uk>
2024-10-18 19:10:06 +0100

    Terrain generation tester

---
+++ GodotExperiments3dTerrainCs/Scenes/TerrainGeneration.cs
@@ -0,0 +1,58 @@
+using Godot;
+
+using System;
+
+namespace GodotExperiments.Terrain3DCSharp;
+
+public partial class TerrainGeneration : Node
+{
+  readonly Random _rng = new();
+
+  [Export]
+  public MeshInstance3D InitialMeshInstance { get; set; }
+
+  public MeshInstance3D ActiveMeshInstance { get; set; } = new MeshInstance3D();
+
+  [Export]
+  public FastNoiseLite MeshNoise { get; set; }
+
+  public override void _Ready()
+  {
+    base._Ready();
+    AddChild(ActiveMeshInstance);
+  }
+
+  public void GenerateMesh()
+  {
+    var surfaceTool = new SurfaceTool();
+    var meshDataTool = new MeshDataTool();
+
+    surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
+    surfaceTool.CreateFrom(InitialMeshInstance.Mesh, 0);
+
+    var arrayPlane = surfaceTool.Commit();
+    meshDataTool.CreateFromSurface(arrayPlane, 0);
+
+    for (var i = 0; i < meshDataTool.GetVertexCount() - 1; i++)
+    {
+      var vertex = meshDataTool.GetVertex(i);
+      vertex.Y = GetNoiseY(vertex.X, vertex.Z);
+      meshDataTool.SetVertex(i, vertex);
+    }
+
+    arrayPlane.ClearSurfaces();
+    meshDataTool.CommitToSurface(arrayPlane);
+
+    surfaceTool.Begin(Mesh.PrimitiveType.Points);
+    surfaceTool.CreateFrom(arrayPlane, 0);
+    surfaceTool.GenerateNormals();
+
+    ActiveMeshInstance.Mesh = surfaceTool.Commit();
+    ActiveMeshInstance.CreateTrimeshCollision();
+  }
+
+  private float GetNoiseY(float x, float y)
+  {
+    return MeshNoise.GetNoise2D(x, y) * 25;
+  }
+}
---
+++ GodotExperiments3dTerrainCs/Scenes/UI/Ui.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using Godot;
+
+namespace GodotExperiments.Terrain3DCSharp.Ui;
+
+public static class ButtonExtensions
+{
+  public static OptionButton WithOptions<TEnum>(this OptionButton button) where TEnum : Enum
+  {
+    var enumValues = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+
+    for (var i = 0; i < enumValues.Count(); i++)
+    {
+      button.AddItem(Enum.GetName(typeof(TEnum), enumValues.ElementAt(i)), i);
+    }
+
+    button.ForceUpdateTransform();
+    return button;
+  }
+}
+
+public partial class Ui : Panel
+{
+  [Export]
+  public TerrainGeneration TerrainGeneration { get; set; }
+
+  OptionButton _noiseTypeControl;
+  SpinBox _noiseSeedControl;
+  SpinBox _noiseFreqControl;
+  
+  OptionButton _noiseFractalTypeControl;
+  SpinBox _noiseFractalLacControl;
+  SpinBox _noiseFractalGainControl;
+  SpinBox _noiseFractalWeightedStressControl;
+
+  OptionButton _noiseCellularFunctionControl;
+  SpinBox _noiseCellularJitterControl;
+  OptionButton _noiseCellularReturnControl;
+
+  CheckBox _noiseDomainWarpEnabledControl;
+  OptionButton _noiseDomainWarpTypeControl;
+  SpinBox _noiseDomainWarpAmplitudeControl;
+  SpinBox _noiseDomainWarpFrequencyControl;
+  
+  OptionButton _noiseDomainWarpFractalTypeControl;
+  SpinBox _noiseDomainWarpFractalOctaveControl;
+  SpinBox _noiseDomainWarpFractalLacControl;
+  SpinBox _noiseDomainWarpFractalGainControl;
+  
+
+  public override void _Ready()
+  {
+    base._Ready();
+
+    _noiseTypeControl                     ??= GetNode<OptionButton>("NoisePanel/NoiseType/Value").WithOptions<FastNoiseLite.NoiseTypeEnum>();
+    _noiseSeedControl                     ??= GetNode<SpinBox>("NoisePanel/NoiseSeed/Value");
+    _noiseFreqControl                     ??= GetNode<SpinBox>("NoisePanel/NoiseFreq/Value");
+
+    _noiseFractalTypeControl              ??= GetNode<OptionButton>("NoisePanel/NoiseFractalType/Value").WithOptions<FastNoiseLite.FractalTypeEnum>();
+    _noiseFractalLacControl               ??= GetNode<SpinBox>("NoisePanel/NoiseFractalLacunarity/Value");
+    _noiseFractalGainControl              ??= GetNode<SpinBox>("NoisePanel/NoiseFractalGain/Value");
+    _noiseFractalWeightedStressControl    ??= GetNode<SpinBox>("NoisePanel/NoiseFractalWeightedStress/Value");
+
+    _noiseCellularFunctionControl         ??= GetNode<OptionButton>("NoisePanel/NoiseCellularFunction/Value").WithOptions<FastNoiseLite.CellularDistanceFunctionEnum>();
+    _noiseCellularJitterControl           ??= GetNode<SpinBox>("NoisePanel/NoiseCellularJitter/Value");
+    _noiseCellularReturnControl           ??= GetNode<OptionButton>("NoisePanel/NoiseCellularReturn/Value");
+
+    _noiseDomainWarpEnabledControl        ??= GetNode<CheckBox>("NoisePanel/NoiseDomainWarpEnabled/Value");
+    _noiseDomainWarpTypeControl           ??= GetNode<OptionButton>("NoisePanel/NoiseDomainWarpType/Value").WithOptions<FastNoiseLite.DomainWarpFractalTypeEnum>();
+    _noiseDomainWarpAmplitudeControl      ??= GetNode<SpinBox>("NoisePanel/NoiseDomainWarpAmplitude/Value");
+    _noiseDomainWarpFrequencyControl      ??= GetNode<SpinBox>("NoisePanel/NoiseDomainWarpFreq/Value");
+
+    _noiseDomainWarpFractalTypeControl    ??= GetNode<OptionButton>("NoisePanel/NoiseDomainWarpFractalType/Value").WithOptions<FastNoiseLite.DomainWarpFractalTypeEnum>();
+    _noiseDomainWarpFractalOctaveControl  ??= GetNode<SpinBox>("NoisePanel/NoiseDomainWarpFractalOctave/Value");
+    _noiseDomainWarpFractalLacControl     ??= GetNode<SpinBox>("NoisePanel/NoiseDomainWarpFractalLacunarity/Value");
+    _noiseDomainWarpFractalGainControl    ??= GetNode<SpinBox>("NoisePanel/NoiseDomainWarpFractalGain/Value");
+  }
+
+
+
+  public void OnUiUpdate()
+  {
+    TerrainGeneration.MeshNoise.NoiseType                    = (FastNoiseLite.NoiseTypeEnum)_noiseTypeControl.Selected;
+    TerrainGeneration.MeshNoise.Seed                         = (int)_noiseSeedControl.Value;
+    TerrainGeneration.MeshNoise.Frequency                    = (float)_noiseFreqControl.Value;
+
+    TerrainGeneration.MeshNoise.FractalType                  = (FastNoiseLite.FractalTypeEnum)_noiseFractalTypeControl.Selected;
+    TerrainGeneration.MeshNoise.FractalLacunarity            = (float)_noiseFractalLacControl.Value;
+    TerrainGeneration.MeshNoise.FractalGain                  = (float)_noiseFractalGainControl.Value;
+    TerrainGeneration.MeshNoise.FractalWeightedStrength      = (float)_noiseFractalWeightedStressControl.Value;
+
+    TerrainGeneration.MeshNoise.CellularDistanceFunction     = (FastNoiseLite.CellularDistanceFunctionEnum)_noiseCellularFunctionControl.Selected;
+    TerrainGeneration.MeshNoise.CellularJitter               = (float)_noiseCellularJitterControl.Value;
+    TerrainGeneration.MeshNoise.CellularReturnType           = (FastNoiseLite.CellularReturnTypeEnum)_noiseCellularReturnControl.Selected;
+
+    TerrainGeneration.MeshNoise.DomainWarpEnabled            = _noiseDomainWarpEnabledControl.ButtonPressed;
+    TerrainGeneration.MeshNoise.DomainWarpType               = (FastNoiseLite.DomainWarpTypeEnum)_noiseDomainWarpTypeControl.Selected;
+    TerrainGeneration.MeshNoise.DomainWarpAmplitude          = (float)_noiseDomainWarpAmplitudeControl.Value;
+    TerrainGeneration.MeshNoise.DomainWarpFrequency          = (float)_noiseDomainWarpFrequencyControl.Value;
+
+    TerrainGeneration.MeshNoise.DomainWarpFractalType        = (FastNoiseLite.DomainWarpFractalTypeEnum)_noiseDomainWarpFractalTypeControl.Selected;
+    TerrainGeneration.MeshNoise.DomainWarpFractalOctaves     = (int)_noiseDomainWarpFractalOctaveControl.Value;
+    TerrainGeneration.MeshNoise.DomainWarpFractalLacunarity  = (float)_noiseDomainWarpFractalLacControl.Value;
+    TerrainGeneration.MeshNoise.DomainWarpFractalGain        = (float)_noiseDomainWarpFractalGainControl.Value;
+    TerrainGeneration.GenerateMesh();
+  }
+}
